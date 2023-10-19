b1e035dd934317b705cd02ef7ce430b17e04b7f5
Atlas Kaplan <aakaplan@proton.me>
2023-10-19 22:42:49 +0100

    0206 Solution: Invoice

--- example_test.go
+++ example_test.go
@@ -1,28 +1,34 @@
 package main
 
 import (
-	"fmt"
+	"testing"
 	"time"
+
+	"github.com/kxplxn/learning_go-grpc/pb"
+
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
-func ExampleInvoice() {
-	// TODO: Change to protobuf invoice
-	inv := Invoice{
-		ID:       "2023-0123",
-		Time:     time.Date(2023, time.January, 7, 13, 45, 0, 0, time.UTC),
+func TestInvoice(t *testing.T) {
+	inv := pb.Invoice{
+		Id: "2023-0123",
+		Time: timestamppb.New(
+			time.Date(2023, time.January, 7, 13, 45, 0, 0, time.UTC),
+		),
 		Customer: "Wile E. Coyote",
-		Items: []LineItem{
-			{SKU: "hammer-20", Amount: 1, Price: 249},
-			{SKU: "nail-9", Amount: 100, Price: 1},
-			{SKU: "glue5", Amount: 1, Price: 799},
+		Items: []*pb.LineItem{
+			{Sku: "hammer-20", Amount: 1, Price: 249},
+			{Sku: "nail-9", Amount: 100, Price: 1},
+			{Sku: "glue5", Amount: 1, Price: 799},
 		},
 	}
-	fmt.Printf("%v\n", &inv)
-	// TODO: encode to []byte using protobuf
-	data, err := []byte(nil), error(nil)
+	t.Logf("%v\n", &inv)
+
+	data, err := proto.Marshal(&inv)
 	if err == nil {
-		fmt.Printf("size: %d\n", len(data))
+		t.Logf("size: %d\n", len(data))
 	} else {
-		fmt.Printf("ERROR: %s\n", err)
+		t.Errorf("ERROR: %s\n", err)
 	}
 }
