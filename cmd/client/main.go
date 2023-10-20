78bda706c8e75c906a31090f1f0e5f7ecba8722d
Atlas Kaplan <aakaplan@proton.me>
2023-10-20 09:17:48 +0100

    0401 Creating a client

---
+++ cmd/client/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/kxplxn/learning_go-grpc/pb"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	addr := "localhost:9292"
+	creds := insecure.NewCredentials()
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	conn, err := grpc.DialContext(
+		ctx,
+		addr,
+		grpc.WithTransportCredentials(creds),
+		grpc.WithBlock(),
+	)
+	if err != nil {
+		log.Fatalf("error: %s", err)
+	}
+	defer func() { _ = conn.Close() }()
+
+	log.Printf("info: connected to %s", addr)
+	c := pb.NewRidesClient(conn)
+	fmt.Println(c)
+}
