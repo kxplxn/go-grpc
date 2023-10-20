3b82be326aea306196b832b1d6303b04f271ca8b
Atlas Kaplan <aakaplan@proton.me>
2023-10-20 09:51:33 +0100

    0402 Calling the server

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/kxplxn/learning_go-grpc/pb"
+	"google.golang.org/protobuf/types/known/timestamppb"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -32,4 +33,24 @@ func main() {
 	log.Printf("info: connected to %s", addr)
 	c := pb.NewRidesClient(conn)
 	fmt.Println(c)
+
+	req := pb.StartRequest{
+		Id:       "47a74960d6204a52b1bece53221eb458",
+		DriverId: "007",
+		Location: &pb.Location{
+			Lat: 51.234125,
+			Lng: -1.124131,
+		},
+		PassengerIds: []string{"M", "Q"},
+		Time:         timestamppb.Now(),
+		Type:         pb.RideType_POOL,
+	}
+
+	ctx, cancel = context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	resp, err := c.Start(ctx, &req)
+	if err != nil {
+		log.Fatalf("error: %s", err)
+	}
+	fmt.Println(resp)
 }
